765ecee44f049b7c3fd938b9a6fe0eeab2e4d682
vikrant <vikrant.negi74@gmail.com>
2021-05-16 10:08:14 +0530

    ADD: ContentScreen

---
+++ disneyplus/src/Screens/MovieScreen/MovieScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './MovieScreen.css'
+
+const MovieScreen = () => {
+    return (
+        <div className="disneyPlus__content">
+            <div className="content__background">
+                <img className="backgroundContent" src="" alt="" />
+            </div>
+        </div>
+    )
+}
+
+export default MovieScreen
