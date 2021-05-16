9c5b398b47bc28c2a491456a869057391055adc2
vikrant <vikrant.negi74@gmail.com>
2021-05-16 10:08:06 +0530

    UPDATE: Router

--- disneyplus/src/App.js
+++ disneyplus/src/App.js
@@ -2,11 +2,28 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header/Header';
 import LandingPage from './Screens/LandingPage/LandingPage';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import MovieScreen from './Screens/MovieScreen/MovieScreen';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/content">
+            <MovieScreen />
+          </Route>
+        </Switch>
+      </Router>
       <LandingPage />
     </div>
   );
