ce401e94097f7159d42013e7ff0c4acf0e2f9bd9
vikrant <vikrant.negi74@gmail.com>
2021-05-17 20:47:39 +0530

    UPDATE: AuthScreen

--- disneyplus/src/Screens/AuthScreen/AuthScreen.js
+++ disneyplus/src/Screens/AuthScreen/AuthScreen.js
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react'
 import './AuthScreen.css'
 
@@ -5,7 +7,12 @@ const AuthScreen = () => {
     return (
         <div className="authScreen">
             <div className="authScreen_content">
-                
+            <img src="/assets/images/cta-logo-one.svg" className="contentAuth__logo" alt="cta-image" />
+                <a className="contentAuth__button">Get All There!</a>
+                <p className="contentAuth__desc">
+                    Get Premier Access to Raya and the Last Dragon for an additional fee with a Disney+ subscription. As of 03/26/21, the price of Disney+ and The Disney Bundle will increase by $1.
+                </p>
+                <img src="/assets/images/cta-logo-two.png" className="contentAuth__logo" alt="cta2-image" />
             </div>
         </div>
     )
