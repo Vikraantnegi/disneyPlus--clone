307404292747788f5887f81e483c56aea17fb177
vikrant <vikrant.negi74@gmail.com>
2021-05-18 10:01:25 +0530

    Refactoring : Brand

--- disneyplus/src/Components/LandingPage/Brands/Brands.js
+++ disneyplus/src/Components/LandingPage/Brands/Brands.js
@@ -1,33 +1,6 @@
 import React from 'react'
 import './Brands.css'
-
-const BrandLogo = [
-    {
-        name: 'Disney',
-        logo: '/assets/images/viewers-disney.png',
-        video: '/assets/videos/1564674844-disney.mp4'
-    },
-    {
-        name: 'Marvel',
-        logo: '/assets/images/viewers-marvel.png',
-        video: '/assets/videos/1564676115-marvel.mp4'
-    },
-    {
-        name: 'NationalTV',
-        logo: '/assets/images/viewers-national.png',
-        video: '/assets/videos/1564676296-national-geographic.mp4'
-    },
-    {
-        name: 'Pixar',
-        logo: '/assets/images/viewers-pixar.png',
-        video: '/assets/videos/1564676714-pixar.mp4'
-    },
-    {
-        name: 'StarWars',
-        logo: '/assets/images/viewers-starwars.png',
-        video: '/assets/videos/1608229455-star-wars.mp4'
-    },
-]
+import {BrandLogo} from '../../../assets/data/BrandData';
 
 const Brands = () => {
     return (
